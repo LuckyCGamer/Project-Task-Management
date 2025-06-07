e4273dfce042be8530643b2f834835da7643e23e
Eric Chandra <erichandra5@gmail.com>
2025-06-07 20:47:47 +0700

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { TaskProvider } from "./component/context/TaskContext";
 import TaskHeader from "./component/TaskHeader";
-import TaskInput from "./component/TaskInput";
 
 function App() {
 
   return (
-    <TaskProvider>
-      <TaskHeader />
-
-      <div className="container mx-auto px-4 py-8">
-        <TaskInput />
-      </div>
-      
-    </TaskProvider>
+    <TaskHeader />
   )
 }
 
--- src/component/TaskHeader.jsx
+++ src/component/TaskHeader.jsx
@@ -1,7 +1,7 @@
 export default function TaskHeader() {
   return (
     <div className="text-center animate-fade-in">
-      <h1 className="text-4xl md:text-5xl font-bold bg-gradient-to-r from-primary-600 via-primary-500 to-primary-700 bg-clip-text text-transparent mb-4">
+      <h1 className="text-4xl md:text-5xl font-bold bg-gradient-to-r from-primary-600 via-primary-500 to-primary-700 bg-clip-text mb-4">
         Task Management
       </h1>
       <p className="text-muted-foreground text-lg max-w-2xl mx-auto">
